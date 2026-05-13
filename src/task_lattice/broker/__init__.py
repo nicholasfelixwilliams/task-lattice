363f10dde61c0697315752d21a7f622a83b297e3
Nicholas Williams <43884023+nicholasfelixwilliams@users.noreply.github.com>
2026-05-13 20:50:59 +0100

    Task registry tests

--- src/task_lattice/broker/__init__.py
+++ src/task_lattice/broker/__init__.py
@@ -1,6 +1,7 @@
 from .base import Broker
+from .in_memory import InMemoryBroker
 from .solace import SolaceBroker
 
-BROKERS = {"solace": SolaceBroker}
+BROKERS = {"in-memory": InMemoryBroker, "solace": SolaceBroker}
 
 __all__ = ["Broker", "BROKERS"]
